1a7b25f70fd5d131a665dffa1d7c8657f7f42df0
CoC DT <test@mail>
2018-08-31 13:46:11 +0200

    added some comments and deleted obsolete imports

--- camera_publish.py
+++ camera_publish.py
@@ -2,10 +2,6 @@
 """ This script is a ros node, that publishes a camera image as Image message named 'image' """
 import rospy
 from sensor_msgs.msg import Image
-from std_msgs.msg import UInt8
-from zal_object_detection.msg import UInts_zal
-
-
 from cv_bridge import CvBridge, CvBridgeError
 try:
     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
@@ -13,23 +9,16 @@ except:
     pass
 import cv2
 import time
-from rospy.numpy_msg import numpy_msg
-import numpy as np
 
+# initialize ROS node and Publisher
 rospy.init_node('VideoPublisher', anonymous=True)
-
 VideoRaw = rospy.Publisher('image', Image, queue_size=1)
-# VideoRaw = rospy.Publisher('image_np', numpy_msg(UInts_zal), queue_size=1)
 
 cam = cv2.VideoCapture('/dev/video1')
 
 while not rospy.is_shutdown():
+    # read the camera image, convert it to an Image Message and publish
     meta, frame = cam.read()
-
     msg_frame = CvBridge().cv2_to_imgmsg(frame)
-
-    # msg_frame = np.asarray(frame, dtype=np.uint8)
-
-    # print(np.ndarray.flatten(msg_frame).shape)
     VideoRaw.publish(msg_frame)
     time.sleep(0.1)
